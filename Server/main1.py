b8c5c203baa362fee084a916a994f0e974734d1e
kathanshah28 <kathanshah2004@gmail.com>
2025-04-13 15:11:23 +0530

    np problem solve 1

--- Server/main1.py
+++ Server/main1.py
@@ -334,7 +334,7 @@ def process_machine_unit(machine_id, row_idx):
             'id': str(machine_id),
             'timestamp': str(test_df['timestamp']),  # Ensure timestamp is string
             'anomaly_score': convert_value(results['anomaly_score']),
-            'predicted_anamoly': str(results['predicted_anomaly']),
+            'predicted_anomaly': str(results['predicted_anomaly']),
             'predicted_anomaly_type': str(results['predicted_anomaly_type']), 
             'predicted_health_score': convert_value(results['predicted_health_score']),
             'predicted_days_to_maintenance': convert_value(results['predicted_days_to_maintenance']),
